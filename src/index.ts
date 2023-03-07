52c8dbb81481c59dde4dd0042a18e0da6765e644
Ferdinand Cuisin <ferdinand.c@meta-api.io>
2023-03-07 15:21:22 +0100

    setup front and back-end

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import Bundler from "parcel-bundler";
+import path from "path";
+
+const app = express();
+const port = 8080 || process.env.PORT;
+
+const bundler = new Bundler(path.join(__dirname, "../src/client/index.html"));
+app.use(bundler.middleware());
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
